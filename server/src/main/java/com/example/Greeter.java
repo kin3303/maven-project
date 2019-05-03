1153641f91dbbf7a1c3191c7ea0236b538d41d52
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-05-03 11:43:05 +0900

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,24 +1,23 @@
 /**
- * Test Info about this package doing something for package-info.java file...
+ * Info about this package doing something for package-info.java file.
  */
 package com.example;
 
 /**
- * This is a class Greeter aaa
+ * This is a class Greeter.
  */
 public class Greeter {
 
   /**
-   * This is a constructor.asdfa 
+   * This is a constructor. 
    */
   public Greeter() {
-
   }
 
   /**
-  * @param someone the name of a person
-  * @return greeting string
-  */
+   * @param someone the name of a person
+   * @return greeting string
+   */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
