fa8fbad50f033cf72350e41538eaeaf5799233b1
kin3303 <48729027+kin3303@users.noreply.github.com>
2019-05-03 11:24:21 +0900

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,5 +1,5 @@
 /**
- * Info about this package doing something for package-info.java file...
+ * Test Info about this package doing something for package-info.java file...
  */
 package com.example;
 
